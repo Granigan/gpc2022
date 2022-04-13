49787c0d3d1ce1f3d9f3aa33a6ab9e3492d465df
Tero Tapio <teromarkustapio@gmail.com>
2022-04-13 21:20:32 +0300

    add game over when timer runs out

--- Assets/Scripts/TMPTimerScript.cs
+++ Assets/Scripts/TMPTimerScript.cs
@@ -7,32 +7,31 @@ using TMPro;
 public class TMPTimerScript : MonoBehaviour
 {
   public float timeRemaining = 10;
-  public bool timerIsRunning = false;
-  public TextMeshProUGUI timerText;
+  private bool timerIsRunning = false;
+  private TextMeshProUGUI timer;
+  private GameObject player;
 
   private void Start()
   {
     // Starts the timer automatically
     timerIsRunning = true;
-    timerText = GetComponent<TextMeshProUGUI>();
+    timer = GetComponent<TextMeshProUGUI>();
+    player = GameObject.Find("Player");
   }
   
   void Update()
   {
-    if (timerIsRunning)
+    if (timerIsRunning && timeRemaining > 0)
     {
-      if (timeRemaining > -1)
-      {
-          timeRemaining -= Time.deltaTime;
-      }
-      else
-      {
-        Debug.Log("Time has run out!");
-        timeRemaining = 0;
-        timerIsRunning = false;
-      }
+      timeRemaining -= Time.deltaTime;
+      DisplayTime(timeRemaining);
+    } else
+    {
+      timeRemaining = 0;
+      timerIsRunning = false;
+      timer.text = "Game over!";
+      player.GetComponent<PlayerScript>().EndGame();
     }
-    DisplayTime(timeRemaining);
   }
   
   void DisplayTime(float timeToDisplay)
@@ -40,7 +39,7 @@ public class TMPTimerScript : MonoBehaviour
     timeToDisplay += 1;
     float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
     float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-    timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
   }
 
   public void AddBonusTime(float timeToAdd) 
